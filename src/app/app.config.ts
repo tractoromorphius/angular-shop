e15f3782599bf6d53804f82ddc2e5e6edb6ce46d
K700 <empty@mail>
2024-10-19 01:50:40 +0300

    install angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection, inject } from '@angular/core';
+import { ApplicationConfig, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,22 +8,23 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { InMemoryCache } from '@apollo/client/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideRouter(routes),
+    provideAnimationsAsync(),
     {provide: ENVIRONMENTS, useValue: environment}, 
     provideHttpClient(),
     provideApollo(() => {
       const httpLink = inject(HttpLink);
       const apiUrl = inject(EnvironmentsService).getEnvironment('backApi');
-
       return {
         link: httpLink.create({
           uri: apiUrl,
         }),
         cache: new InMemoryCache(),
       };
-    }),
+    }), 
   ]
 };
