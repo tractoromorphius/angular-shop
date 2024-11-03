d49dc45a75420f8fbe3884e21ad4130204653b6d
K700 <empty@mail>
2024-11-03 16:26:47 +0300

    creaate product-card component

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'shop-products-list',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductCardComponent,
+    NgForOf,
+  ],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css'
 })
