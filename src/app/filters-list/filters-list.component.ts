5a79fe1b13566fe332252ed31402320f4657e7bc
K700 <empty@mail>
2024-11-03 09:46:42 +0300

    main layout setup

---
+++ src/app/filters-list/filters-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shop-filters-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './filters-list.component.html',
+  styleUrl: './filters-list.component.css'
+})
+export class FiltersListComponent {
+
+}
---
+++ src/app/products-list/products-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shop-products-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-list.component.html',
+  styleUrl: './products-list.component.css'
+})
+export class ProductsListComponent {
+
+}
